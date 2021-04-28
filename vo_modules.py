52389ebdc0abb58e2a14e2fccc51fa5aa2476d0b
yamaru12345 <65715903+yamaru12345@users.noreply.github.com>
2021-04-28 11:09:45 +0900

    Update vo_modules.py

--- vo_modules.py
+++ vo_modules.py
@@ -1075,6 +1075,7 @@ class VisualOdometry():
             else:
                 raise NotImplementedError
             self.timers.timers["tracking"].append(time()-start_time)
+            np.save(f'{result/flow_{img_id}.npy}, ref_data["flow"])
 
             """ Visualization """
             start_time = time()
